eb2f5d95f76a77aff322d77ec97a669bb519fa8c
NETHRA <nethra200709@gmail.com>
2026-01-31 16:39:37 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Grade{
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int score=sc.nextInt();
+      if (score >= 90) {
+          System.out.println("Grade: A");
+     } else if (score >= 80) {
+         System.out.println("Grade: B");
+    } else if (score >= 70) {
+        System.out.println("Grade: C");
+    } else if (score >= 60) {
+       System.out.println("Grade: D");
+    } else {
+       System.out.println("Grade: F");
+    }
+  }
+}
