b6bcce2598df0f51b360464f72a09f5ed5c8fb09
NETHRA <nethra200709@gmail.com>
2026-01-31 16:38:06 +0530

    Add files via upload

---
+++ note.java
@@ -0,0 +1,10 @@
+public class note{
+  public static void main(String args[]){
+    System.out.println("welcome to java");
+    int a=5;
+    int b=6;
+    int c;
+    c=a+b;
+    System.out.println("the sum of a and b is:"+c);
+  } 
+}                                                
