96a0fc3c47e8db61972a69260831361ff0f1d60e
NETHRA <nethra200709@gmail.com>
2026-02-09 17:21:43 +0530

    Add files via upload

---
+++ StopAtFifthteen.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class StopAtFifthteen{
+  public static void main(String[] args){
+    for(int i=1;i<=20;i++){
+      if(i==15){
+        break;
+      }
+      System.out.println(i);
+    } } }
