22fcc5b2c161d218e9752b4a0da814a3566de244
NETHRA <nethra200709@gmail.com>
2026-02-19 16:57:48 +0530

    Add files via upload

--- Details.java
+++ Details.java
@@ -1,38 +1,60 @@
 import java.util.Scanner;
 
-class Stud {
-    int sno;
+class Student {
     String name;
+    int register;
     int age;
     String department;
-    int mark1;
-    int mark2;
-    int total;
 
-    
-    Stud(int a, String b, int c, String d, int e, int f) {
-        sno = a;
-        name = b;
+    // Constructor
+    Student(String a, int b, int c, String d) {
+        name = a;
+        register = b;
         age = c;
         department = d;
-        mark1 = e;
-        mark2 = f;
     }
 
-    
-    void calculate() {
-        total = mark1 + mark2;
-    }
-
-    
-    void display() {
-        System.out.println("Register no: " + sno);
+    void disp1() {
         System.out.println("Name: " + name);
+        System.out.println("Register: " + register);
         System.out.println("Age: " + age);
         System.out.println("Department: " + department);
+    }
+}
+
+class Mark extends Student {
+    int mark1, mark2, mark3, total;
+    float average;
+    String result;
+
+    // Constructor with super
+    Mark(String name, int register, int age, String department,
+         int m1, int m2, int m3) {
+
+        super(name, register, age, department);
+        mark1 = m1;
+        mark2 = m2;
+        mark3 = m3;
+    }
+
+    void calculate() {
+        total = mark1 + mark2 + mark3;
+        average = total / 3.0f;
+
+        if (mark1 >= 40 && mark2 >= 40 && mark3 >= 40) {
+            result = "Pass";
+        } else {
+            result = "Fail";
+        }
+    }
+
+    void disp2() {
         System.out.println("Mark1: " + mark1);
         System.out.println("Mark2: " + mark2);
+        System.out.println("Mark3: " + mark3);
         System.out.println("Total: " + total);
+        System.out.println("Average: " + average);
+        System.out.println("Result: " + result);
     }
 }
 
@@ -41,28 +63,30 @@ public class Details {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Register No:");
-        int sno = sc.nextInt();
-
-        System.out.println("Enter Name:");
+        System.out.println("ENTER NAME:");
         String name = sc.next();
 
-        System.out.println("Enter Age:");
+        System.out.println("ENTER REGISTRATION NUMBER:");
+        int register = sc.nextInt();
+
+        System.out.println("ENTER AGE:");
         int age = sc.nextInt();
 
-        System.out.println("Enter Department:");
+        System.out.println("ENTER DEPARTMENT:");
         String department = sc.next();
 
-        System.out.println("Enter Mark1:");
+        System.out.println("ENTER THREE MARKS:");
         int mark1 = sc.nextInt();
-
-        System.out.println("Enter Mark2:");
         int mark2 = sc.nextInt();
+        int mark3 = sc.nextInt();
 
-        Stud s1 = new Stud(sno, name, age, department, mark1, mark2 );
-        s1.calculate();
-        s1.display();
+        Mark mar = new Mark(name, register, age, department,
+                            mark1, mark2, mark3);
 
-        
+        mar.calculate();
+        mar.disp1();
+        mar.disp2();
+
+        sc.close();
     }
 }
