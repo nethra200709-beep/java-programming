4b5b22c3bd809db317a6ebc3dc8ae07f84b8f633
NETHRA <nethra200709@gmail.com>
2026-02-27 17:21:05 +0530

    Add files via upload

---
+++ SleepDemo.java
@@ -0,0 +1,18 @@
+class SleepDemo extends Thread {
+
+    public void run() {
+        try {
+            System.out.println("Thread going to sleep");
+            Thread.sleep(3000);  
+            System.out.println("Thread woke up");
+        } 
+        catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        SleepDemo s = new SleepDemo();
+        s.start();
+    }
+}
